d3491b06b165dc02130af9a840d5219c65a12456
Xzot <shchukavlad4@gmail.com>
2018-10-16 00:29:00 +0300

    Add xcode project into repo

---
+++ Youtube/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Youtube
+//
+//  Created by Vlad Shchuka on 10/16/18.
+//  Copyright © 2018 Vlad Shchuka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
