edfe1963964d489784aa135fa60b7dc9864e1d25
Xzot <shchukavlad4@gmail.com>
2018-10-16 10:01:32 +0300

    Add Options structure for VideoInfoProvider confiqurations

---
+++ Youtube/Info Providers/Video/Provider Confiq/Options.swift
@@ -0,0 +1,44 @@
+
+import Foundation
+import ObjectMapper
+
+struct Options {
+
+    enum Part: String {
+        case id
+    }
+
+    enum `Type`: String {
+        case video
+    }
+
+    let part: Part
+    let maxResults: Int
+    let q: String
+    let type: Type
+    var key: String {
+        return "AIzaSyC-O4GFWLZLuK69qJNTkAQmeiFErtt88jw"
+    }
+    let nextPageToken: String?
+
+    init(_ part: Part = .id, maxResults: Int = 10, q: String = "", type: Type = .video, nextPageToken: String? = nil ) {
+        self.part = part
+        self.maxResults = maxResults
+        self.q = q
+        self.type = type
+        self.nextPageToken = nextPageToken
+    }
+}
+
+extension Options: BaseMappable {
+
+    mutating func mapping(map: Map) {
+        part >>> map["part"]
+        maxResults >>> map["maxResults"]
+        q >>> map["q"]
+        type >>> map["type"]
+        key >>> map["key"]
+        nextPageToken >>> map["nextPageToken"]
+    }
+
+}
--- Youtube/Info Providers/Video/Provider/VideoInfoProvider.swift
+++ Youtube/Info Providers/Video/Provider/VideoInfoProvider.swift
@@ -1,7 +1,6 @@
 
 import Foundation
 import Alamofire
-import ObjectMapper
 import AlamofireObjectMapper
 
 final class VideoInfoProvider {
@@ -11,25 +10,21 @@ final class VideoInfoProvider {
 
     func fetchVideos(by query: String, with success: @escaping (_ videos: [VideoInfo]) -> Void, or failure: @escaping (_ error: Error) -> Void) {
         fetchVideoData(by: query, with: { [weak self] (response) in
+            guard let `self` = self else { return }
             guard let nextPageToken = response.nextPageToken else {
-                self?.isLastPageLoaded = true
+                self.isLastPageLoaded = true
+                self.nextPageToken = nil
                 success(response.items)
                 return
             }
-            self?.nextPageToken = nextPageToken
+            self.nextPageToken = nextPageToken
             success(response.items)
         }, or: failure)
     }
 
     private func fetchVideoData(by query: String, with success: @escaping (_ response: VideosResponse) -> Void, or failure: @escaping (_ error: Error) -> Void) {
-        let options = [
-            "part" : "id",
-            "maxResults" : "10",
-            "q" : query,
-            "type" : "video",
-            "key" : "AIzaSyC-O4GFWLZLuK69qJNTkAQmeiFErtt88jw"
-        ]
-        SessionManager.default.request("https://www.googleapis.com/youtube/v3/search", method: .get, parameters: options, encoding: URLEncoding.methodDependent, headers: nil).responseObject { (response: DataResponse<VideosResponse>) in
+        let options = Options(nextPageToken: nextPageToken)
+        SessionManager.default.request("https://www.googleapis.com/youtube/v3/search", method: .get, parameters: options.toJSON(), encoding: URLEncoding.methodDependent, headers: nil).responseObject { (response: DataResponse<VideosResponse>) in
             switch response.result {
             case .success(let responseValue):
                 success(responseValue)
